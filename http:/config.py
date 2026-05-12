e22e1fc747288c0f1bb029b280b756c98a1a65d6
sviru0048-netizen <sviru0048@gmail.com>
2026-05-12 11:43:35 +0530

    Add initial configuration for bot settings

---
+++ http:/config.py
@@ -0,0 +1,8 @@
+# KRISH X STAR CODER - Config File
+
+BOT_NAME = "KRISH X STAR CODER"
+SUPPORT_CHANNEL = "https://t.me/KRISH_HACKER_OP"
+
+API_ID = 123456
+API_HASH = "your_api_hash_here"
+BOT_TOKEN = "your_bot_token_here"
