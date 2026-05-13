ebf6dc2d15821cb146598b05d518065ffe90c16f
sviru0048-netizen <sviru0048@gmail.com>
2026-05-13 13:37:08 +0530

    Add copyright and developer information
    Added copyright notice and developer information.

--- krishxstar/krishxstar-core/__init__.py
+++ krishxstar/krishxstar-core/__init__.py
@@ -1,1 +1,7 @@
-
+# --------------------------------------------------------------------------------
+#  krishxmusic © 2026
+#  Developed by KRISH X STAR CODER ❤️
+#
+#  Unauthorized copying, editing, re-uploading or removing credits
+#  from this source code is strictly prohibited.
+# --------------------------------------------------------------------------------
