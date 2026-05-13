8358b0d20c38cffbf463cda612fa5d9efce76bad
sviru0048-netizen <sviru0048@gmail.com>
2026-05-13 14:01:18 +0530

    Add module initialization in __init__.py
    Initialize module list for KrishXStarMusic.

---
+++ krishxstar/modules/__init__.py
@@ -0,0 +1,20 @@
+# --------------------------------------------------------------------------------
+#  KrishXStarMusic © 2026
+#  Developed by KRISH X STAR CODER ❤️
+#
+#  Unauthorized copying, editing, re-uploading or removing credits
+#  from this source code is strictly prohibited.
+# --------------------------------------------------------------------------------
+
+ALL_MODULES = [
+    "bot",
+    "play",
+    "pause",
+    "resume",
+    "skip",
+    "stop",
+    "ping",
+    "broadcast",
+    "callbacks",
+    "start",
+]
