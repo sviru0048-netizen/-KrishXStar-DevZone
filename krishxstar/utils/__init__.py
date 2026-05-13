a90840907d9c8eb63c31a428fd3c995859d82ae8
sviru0048-netizen <sviru0048@gmail.com>
2026-05-13 14:47:22 +0530

    Update header and import path in __init__.py

--- krishxstar/utils/__init__.py
+++ krishxstar/utils/__init__.py
@@ -1,11 +1,11 @@
 # --------------------------------------------------------------------------------
-#  krishxstat © 2026
-#  Developed by krish x stat❤️
+#  KrishXStarMusic © 2026
+#  Developed by KRISH X STAR CODER ❤️
 #
 #  Unauthorized copying, editing, re-uploading or removing credits
 #  from this source code is strictly prohibited.
 # --------------------------------------------------------------------------------
 
-from ShizuMusic.utils.permissions import is_user_authorized
+from KrishXStarMusic.utils.permissions import is_user_authorized
 
 __all__ = ["is_user_authorized"]
