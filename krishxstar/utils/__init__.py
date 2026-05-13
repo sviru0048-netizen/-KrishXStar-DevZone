ce5eb0c704c80af7144b5f36f1692d5a399c1400
sviru0048-netizen <sviru0048@gmail.com>
2026-05-13 14:46:14 +0530

    Add licensing comments and update __all__ exports
    Added licensing information and updated exports.

---
+++ krishxstar/utils/__init__.py
@@ -0,0 +1,11 @@
+# --------------------------------------------------------------------------------
+#  krishxstat © 2026
+#  Developed by krish x stat❤️
+#
+#  Unauthorized copying, editing, re-uploading or removing credits
+#  from this source code is strictly prohibited.
+# --------------------------------------------------------------------------------
+
+from ShizuMusic.utils.permissions import is_user_authorized
+
+__all__ = ["is_user_authorized"]
